56e431faf7c590085839a94b78cff963f52ede9e
Craig Demyanovich <craig@8thlight.com>
2011-06-03 00:20:20 -0400

    List of champions

--- src/whowonthecupin/view/index.hiccup.clj
+++ src/whowonthecupin/view/index.hiccup.clj
@@ -1,18 +1,488 @@
-[:div {:style "text-align: center;"}
- [:img {:src "/images/gaeshi.png"}]
- [:h1 "Welcomes You!"]]
+[:div#body_content
 
-[:h3 "Intro"]
-[:p "Gaeshi is a clojure framework for building Google AppEngine sites. The project home is at "
- [:a {:href "https://github.com/slagyr/gaeshi"} "https://github.com/slagyr/gaeshi"] "."
- "It consists of 3 parts"
- [:ul
-  [:li [:b "Kuzushi"] "- which means 'breaking balance', is a Leiningen plugin that supplies all the Geashi commands."]
-  [:li [:b "Tsukuri"] "- which means 'entry', is a library containing all the development tools."]
-  [:li [:b "Kake"] "- which means 'execution', is the runtime library that contains all the main APIs."]]]
+[:h1 "Stanley Cup Champions"]
 
-[:h3 "License"]
-[:p "Copyright (C) 2011 Micah Martin All Rights Reserved."]
-[:p "Distributed under the The MIT License."]
-
-[:p "This app was generated for whowonthecupin."]
+[:table#winners
+[:tbody
+[:tr
+[:td.year "2010"]
+[:td "Chicago Blackhawks"]
+]
+[:tr
+[:td.year "2009"]
+[:td "Pittsburgh Penguins"]
+]
+[:tr
+[:td.year "2008"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "2007"]
+[:td "Anaheim Ducks"]
+]
+[:tr
+[:td.year "2006"]
+[:td "Carolina Hurricanes"]
+]
+[:tr
+[:td.year "2005"]
+[:td.note "Season cancelled due to lockout"]
+]
+[:tr
+[:td.year "2004"]
+[:td "Tampa Bay Lightning"]
+]
+[:tr
+[:td.year "2003"]
+[:td "New Jersey Devils"]
+]
+[:tr
+[:td.year "2002"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "2001"]
+[:td "Colorado Avalanche"]
+]
+[:tr
+[:td.year "2000"]
+[:td "New Jersey Devils"]
+]
+[:tr
+[:td.year "1999"]
+[:td "Dallas Stars"]
+]
+[:tr
+[:td.year "1998"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1997"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1996"]
+[:td "Colorado Avalanche"]
+]
+[:tr
+[:td.year "1995"]
+[:td "New Jersey Devils"]
+]
+[:tr
+[:td.year "1994"]
+[:td "New York Rangers"]
+]
+[:tr
+[:td.year "1993"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1992"]
+[:td "Pittsburgh Penguins"]
+]
+[:tr
+[:td.year "1991"]
+[:td "Pittsburgh Penguins"]
+]
+[:tr
+[:td.year "1990"]
+[:td "Edmonton Oilers"]
+]
+[:tr
+[:td.year "1989"]
+[:td "Calgary Flames"]
+]
+[:tr
+[:td.year "1988"]
+[:td "Edmonton Oilers"]
+]
+[:tr
+[:td.year "1987"]
+[:td "Edmonton Oilers"]
+]
+[:tr
+[:td.year "1986"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1985"]
+[:td "Edmonton Oilers"]
+]
+[:tr
+[:td.year "1984"]
+[:td "Edmonton Oilers"]
+]
+[:tr
+[:td.year "1983"]
+[:td "New York Islanders"]
+]
+[:tr
+[:td.year "1982"]
+[:td "New York Islanders"]
+]
+[:tr
+[:td.year "1981"]
+[:td "New York Islanders"]
+]
+[:tr
+[:td.year "1980"]
+[:td "New York Islanders"]
+]
+[:tr
+[:td.year "1979"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1978"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1977"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1976"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1975"]
+[:td "Philadelphia Flyers"]
+]
+[:tr
+[:td.year "1974"]
+[:td "Philadelphia Flyers"]
+]
+[:tr
+[:td.year "1973"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1972"]
+[:td "Boston Bruins"]
+]
+[:tr
+[:td.year "1971"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1970"]
+[:td "Boston Bruins"]
+]
+[:tr
+[:td.year "1969"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1968"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1967"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1966"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1965"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1964"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1963"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1962"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1961"]
+[:td "Chicago Blackhawks"]
+]
+[:tr
+[:td.year "1960"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1959"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1958"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1957"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1956"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1955"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1954"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1953"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1952"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1951"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1950"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1949"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1948"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1947"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1946"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1945"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1944"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1943"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1942"]
+[:td "Toronto  Maple Leafs"]
+]
+[:tr
+[:td.year "1941"]
+[:td "Boston Bruins"]
+]
+[:tr
+[:td.year "1940"]
+[:td "New York Rangers"]
+]
+[:tr
+[:td.year "1939"]
+[:td "Boston Bruins"]
+]
+[:tr
+[:td.year "1938"]
+[:td "Chicago Blackhawks"]
+]
+[:tr
+[:td.year "1937"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1936"]
+[:td "Detroit Red Wings"]
+]
+[:tr
+[:td.year "1935"]
+[:td "Montreal Maroons"]
+]
+[:tr
+[:td.year "1934"]
+[:td "Chicago Black Hawks"]
+]
+[:tr
+[:td.year "1933"]
+[:td "New York Rangers"]
+]
+[:tr
+[:td.year "1932"]
+[:td "Toronto Maple Leafs"]
+]
+[:tr
+[:td.year "1931"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1930"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1929"]
+[:td "Boston Bruins"]
+]
+[:tr
+[:td.year "1928"]
+[:td "New York Rangers"]
+]
+[:tr
+[:td.year "1927"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td ]
+[:td.note "The NHL assumed control of Stanley Cup competition after 1926"]
+]
+[:tr
+[:td.year "1926"]
+[:td "Montreal Maroons"]
+]
+[:tr
+[:td.year "1925"]
+[:td "Victoria Cougars"]
+]
+[:tr
+[:td.year "1924"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1923"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td.year "1922"]
+[:td "Toronto St. Pats"]
+]
+[:tr
+[:td.year "1921"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td.year "1920"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td.year "1919"]
+[:td.note "No decision"
+  [:br ]
+  "Series beween Montreal and Seattle cancelled due to influenza epidemic"
+]
+]
+[:tr
+[:td.year "1918"]
+[:td "Toronto Arenas"]
+]
+[:tr
+[:td.year "1917"]
+[:td "Seattle Metropolitans"]
+]
+[:tr
+[:td.year "1916"]
+[:td "Montreal Canadiens"]
+]
+[:tr
+[:td.year "1915"]
+[:td "Vancouver Millionaires"]
+]
+[:tr
+[:td.year "1914"]
+[:td "Toronto Blueshirts"]
+]
+[:tr
+[:td.year "1913"]
+[:td "Quebec Bulldogs"]
+]
+[:tr
+[:td.year "1912"]
+[:td "Quebec Bulldogs"]
+]
+[:tr
+[:td.year "1911"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td.year "1910"]
+[:td "Montreal Wanderers, Ottawa Senators"]
+]
+[:tr
+[:td.year "1909"]
+[:td "Ottawa Senators"]
+]
+[:tr
+[:td.year "1908"]
+[:td "Montreal Wanderers"]
+]
+[:tr
+[:td.year "1907"]
+[:td "Montreal Wanderers, Kenora Thistles"]
+]
+[:tr
+[:td.year "1906"]
+[:td "Montreal Wanderers, Ottawa Silver Seven"]
+]
+[:tr
+[:td.year "1905"]
+[:td "Ottawa Silver Seven"]
+]
+[:tr
+[:td.year "1904"]
+[:td "Ottawa Silver Seven"]
+]
+[:tr
+[:td.year "1903"]
+[:td "Ottawa Silver Seven, Montreal AAA"]
+]
+[:tr
+[:td.year "1902"]
+[:td "Montreal AAA, Winnipeg Victorias"]
+]
+[:tr
+[:td.year "1901"]
+[:td "Winnipeg Victorias"]
+]
+[:tr
+[:td.year "1900"]
+[:td "Montreal Shamrocks"]
+]
+[:tr
+[:td.year "1899"]
+[:td "Montreal Shamrocks, Montreal Victorias"]
+]
+[:tr
+[:td.year "1898"]
+[:td "Montreal Victorias"]
+]
+[:tr
+[:td.year "1897"]
+[:td "Montreal Victorias"]
+]
+[:tr
+[:td.year "1896"]
+[:td "Montreal Victorias, Winnipeg Victorias"]
+]
+[:tr
+[:td.year "1895"]
+[:td "Montreal Victorias"]
+]
+[:tr
+[:td.year "1894"]
+[:td "Montreal AAA"]
+]
+[:tr
+[:td.year "1893"]
+[:td "Montreal AAA"]
+]
+]
+]
+]
